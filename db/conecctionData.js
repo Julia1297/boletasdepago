ff02d9bb9db98af7f645d3e9186a54673c686cb7
edwinat <edwinatahuichi92@gmail.com>
2019-04-16 09:50:46 -0400

    bd actualizado

--- db/conecctionData.js
+++ db/conecctionData.js
@@ -2,12 +2,12 @@
 const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './wea.db',
-    define: {
-        timestamps: false
-    }
-  });
+  dialect: 'sqlite',
+  storage: './wea.db',
+  define: {
+    timestamps: false
+  }
+});
 
 sequelize.authenticate()
   .then(() => {
@@ -18,25 +18,27 @@ sequelize.authenticate()
   })
 
 
-  const Tarjeta = sequelize.define('tarjetas', {
-    id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    montoVendido: Sequelize.INTEGER,
-    fecha: Sequelize.STRING,
+const Tarjeta = sequelize.define('tarjetas', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  montoVendido: Sequelize.INTEGER,
+  fecha: Sequelize.STRING,
+  ciEmpleado: Sequelize.INTEGER
+})
+
+Tarjeta.create({ montoVendido: 100, fecha: "Doe", ciEmpleado: 1 });
+
+
+
+Tarjeta.findAll({ attributes: ['id', 'montoVendido', 'fecha', 'ciEmpleado'] })
+  .then(tarjetas => {
+    console.log(JSON.stringify(tarjetas))
+  })
+  .catch(err => {
+    console.log(err)
   })
 
-  Tarjeta.create({ montoVendido: 100, fecha: "Doe" }); 
-
-  
-
-Tarjeta.findAll({ attributes: ['id','montoVendido', 'fecha'] })
-.then(tarjetas => {
-  console.log(JSON.stringify(tarjetas))
-})
-.catch(err => {
-  console.log(err)
-})
 
