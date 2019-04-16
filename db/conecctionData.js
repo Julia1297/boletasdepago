838aa0ab89a179b44085959a8c51077742e01d75
alvaro2302 <alvaro230297@hotmail.com>
2019-04-16 02:07:38 -0400

    add getById

--- db/conecctionData.js
+++ db/conecctionData.js
@@ -1,8 +1,9 @@
 const sqlite3 = require('sqlite3');
 let db = new sqlite3.Database("./wea.db");
 const insertData = () =>{
-    console.log("Insert data")
-    db.run('INSERT INTO TajetaDeVenta (montoVendido,fecha) VALUES (1900,"25/4/2019")');
+    console.log("Insert data");
+    db.run('INSERT INTO TajetaDeVenta (montoVendido,fecha) VALUES (1900,"25/5/2019")');
 }
+
 insertData();
 db.close();
