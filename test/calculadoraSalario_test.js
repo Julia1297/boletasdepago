068b58c8d4945bda89f8a094da6247003c99b65c
wildercossio <cwilrodz@gmail.com>
2019-04-21 14:21:48 -0400

    ordenando tests

--- test/calculadoraSalario_test.js
+++ test/calculadoraSalario_test.js
@@ -8,23 +8,37 @@ import TarjetaVenta from '../tarjetas/tarjetaVenta';
 
 describe('Calculadora de salario', function () {
 
-    it('calcular salario fijo', function () {
+    it('calcular salario para un empleado fijo', function () {
         let calculadora = new CalculadoraPorFijo(1800);
         expect(calculadora.calcularSalario()).equal(1800);
     });
 
-    it('calcular salario por hora', function () {
+    it('calcular el salario para un empleado por hora con 1 tarjeta de hora', function () {
         let tarjetaHora1 = new TarjetaHora("2018-03-22", "08:00:00", "12:00:00");
-        let tarjetaHora2 = new TarjetaHora("2018-03-22", "15:00:00", "18:00:00");
-        let calculadora = new CalculadoraPorHora(200, [tarjetaHora1, tarjetaHora2]);
-        expect(calculadora.calcularSalario()).equal(1400);
+        let calculadora = new CalculadoraPorHora(200, [tarjetaHora1]);
+        expect(calculadora.calcularSalario()).equal(800);
     });
 
-    it('calcular salario con 10% de comision', function () {
+    it('calcular el salario para un empleado por hora con mas de 1 tarjeta de hora', function () {
+        let tarjetaHora1 = new TarjetaHora("2018-03-22", "08:00:00", "12:00:00");
+        let tarjetaHora2 = new TarjetaHora("2018-03-22", "15:00:00", "18:00:00");
+        let tarjetaHora3 = new TarjetaHora("2018-03-23", "15:00:00", "19:00:00");
+        let calculadora = new CalculadoraPorHora(200, [tarjetaHora1, tarjetaHora2, tarjetaHora3]);
+        expect(calculadora.calcularSalario()).equal(2200);
+    });
+    
+    it('calcular el salario para un empleado por comision con 10% de comision y 1 tarjeta de venta', function () {
         let tarjetaVenta1 = new TarjetaVenta(500, "2018-03-22");
-        let tarjetaVenta2 = new TarjetaVenta(500, "2018-03-23");
-        let calculadora = new CalculadoraPorComision(1000, 0.10, [tarjetaVenta1, tarjetaVenta2]);
-        expect(calculadora.calcularSalario()).equal(1100);
+        let calculadora = new CalculadoraPorComision(1000, 0.10, [tarjetaVenta1]);
+        expect(calculadora.calcularSalario()).equal(1050);
+    });
+    
+    it('calcular el salario para un empleado por comision con 10% de comision y mas de 1 tarjeta de venta', function () {
+        let tarjetaVenta1 = new TarjetaVenta(500, "2018-03-22");
+        let tarjetaVenta2 = new TarjetaVenta(900, "2018-03-23");
+        let tarjetaVenta3 = new TarjetaVenta(300, "2018-03-23");
+        let calculadora = new CalculadoraPorComision(1000, 0.10, [tarjetaVenta1, tarjetaVenta2, tarjetaVenta3]);
+        expect(calculadora.calcularSalario()).equal(1170);
     });
 
 });
--- test/tarjetaHora_test.js
+++ test/tarjetaHora_test.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect
 import TarjetaHora from '../tarjetas/tarjetaHora';
 
 describe('',function(){
-    it('calcular cantidad de horas de una Tarjeta de horas', function () {
+    it('calcular cantidad de horas de trabajo de una Tarjeta de hora', function () {
         let tarjetaHora = new TarjetaHora("2018-03-22", "08:00:00", "14:00:00");
         expect(tarjetaHora.obtenerCantidadDeHorasTrabajadas()).equal(6);
     });
