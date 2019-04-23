2dd6fd5597265c26e8fe3d5f7286acabedb78566
alvaro2302 <alvaro230297@hotmail.com>
2019-04-23 09:11:42 -0400

    add database mongo

--- server.js
+++ server.js
@@ -1,28 +1,73 @@
-// const path = require('path');
-// const express = require('express');
-// const app = express();
-// const morgan = require('morgan');
 const mongoose = require('mongoose');
-
-//connect to db
-mongoose.connect('mongodb://localhost/BoletasBd', { useNewUrlParser: true })
+class Email {
+ constructor(){
+    this.mongoose.connect('mongodb://localhost/BoletasBd', { useNewUrlParser: true })
     .then(db => console.log("DB conectada"))
     .catch(err => console.log(err));
+    this.url = 'mongodb://localhost/BoletasBd';
 
-//import routes
-// const routesApp = require('./src/routes/routesApp');
 
-//settings
-// app.set('port', process.env.PORT ||3000);
-// app.set('views',path.join(__dirname,'views'));
-// app.set('view engine', 'ejs')
-//middlewares
-// app.use(morgan('dev'));
-// app.use(express.urlencoded({extended: false}));
-//routes
-// app.use('/', routesApp);
+ }
+//connect to db
 
-//starting the server
-// app.listen(app.get('port'),()=>{
-//     console.log(`Server on port ${app.get('port')}`);
-// });
+
+create(){
+    this.mongoose.connect(this.url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        var myobj = { name: "Company Inc", address: "Highway 37" };
+        dbo.collection("customers").insertOne(myobj, function(err, res) {
+          if (err) throw err;
+          console.log("1 document inserted");
+          db.close();
+        });
+      });
+}
+
+
+
+readOne(){
+    this.mongoose.connect(this.url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    dbo.collection("customers").findOne({}, function(err, result) {
+        if (err) throw err;
+        console.log(result.name);
+        db.close();
+    });
+    });
+}
+
+
+
+Query(){
+    this.mongoose.connect(this.url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    var query = { address: "Park Lane 38" };
+    dbo.collection("customers").find(query).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+    });
+    });
+}
+
+Delete(){
+
+    this.mongoose.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        var myquery = { address: 'Mountain 21' };
+        dbo.collection("customers").deleteOne(myquery, function(err, obj) {
+          if (err) throw err;
+          console.log("1 document deleted");
+          db.close();
+        });
+      });
+}
+
+}
+
+
+
