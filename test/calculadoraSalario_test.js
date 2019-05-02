0a25c4e7c62283234379b593431248bb854a4abb
wildercossio <cwilrodz@gmail.com>
2019-05-01 20:44:57 -0400

    cambiando los test , resultados esperados mayo

--- test/calculadoraSalario_test.js
+++ test/calculadoraSalario_test.js
@@ -10,27 +10,27 @@ let DiasTrabajados=[];
 describe('Calculadora de salario', function () {
 
     it('calcular salario para un empleado fijo que asistio un dia laboral del mes', function () {
-        let tarjetaAsistencia = new TarjetaAsistencia("2018-03-22");
+        let tarjetaAsistencia = new TarjetaAsistencia("2019-03-22");
         let calculadora = new CalculadoraPorFijo(1800,[tarjetaAsistencia]);
-        expect(calculadora.calcularSalario()).equal(81);
+        expect(calculadora.calcularSalario()).equal(78);
     });
 
     it('calcular salario para un empleado fijo que asistio 10 dias laborales del mes', function () {
         DiezDiasTrabajados();
         let calculadora = new CalculadoraPorFijo(1800,DiasTrabajados);
-        expect(calculadora.calcularSalario()).equal(818);
+        expect(calculadora.calcularSalario()).equal(782);
     });
     function DiezDiasTrabajados(){
-        let tarjetaAsistencia1 = new TarjetaAsistencia("2018-03-2");
-        let tarjetaAsistencia2 = new TarjetaAsistencia("2018-03-3");
-        let tarjetaAsistencia3 = new TarjetaAsistencia("2018-03-5");
-        let tarjetaAsistencia4 = new TarjetaAsistencia("2018-03-8");
-        let tarjetaAsistencia5 = new TarjetaAsistencia("2018-03-12");
-        let tarjetaAsistencia6 = new TarjetaAsistencia("2018-03-15");
-        let tarjetaAsistencia7 = new TarjetaAsistencia("2018-03-16");
-        let tarjetaAsistencia8 = new TarjetaAsistencia("2018-03-17");
-        let tarjetaAsistencia9 = new TarjetaAsistencia("2018-03-18");
-        let tarjetaAsistencia10 = new TarjetaAsistencia("2018-03-19");
+        let tarjetaAsistencia1 = new TarjetaAsistencia("2019-03-2");
+        let tarjetaAsistencia2 = new TarjetaAsistencia("2019-03-3");
+        let tarjetaAsistencia3 = new TarjetaAsistencia("2019-03-5");
+        let tarjetaAsistencia4 = new TarjetaAsistencia("2019-03-8");
+        let tarjetaAsistencia5 = new TarjetaAsistencia("2019-03-12");
+        let tarjetaAsistencia6 = new TarjetaAsistencia("2019-03-15");
+        let tarjetaAsistencia7 = new TarjetaAsistencia("2019-03-16");
+        let tarjetaAsistencia8 = new TarjetaAsistencia("2019-03-17");
+        let tarjetaAsistencia9 = new TarjetaAsistencia("2019-03-18");
+        let tarjetaAsistencia10 = new TarjetaAsistencia("2019-03-19");
         DiasTrabajados=[tarjetaAsistencia1,tarjetaAsistencia2,tarjetaAsistencia3,tarjetaAsistencia4,
                         tarjetaAsistencia5,tarjetaAsistencia6,tarjetaAsistencia7,tarjetaAsistencia8,
                         tarjetaAsistencia9,tarjetaAsistencia10];
@@ -51,15 +51,15 @@ describe('Calculadora de salario', function () {
     });
     
     it('calcular el salario para un empleado por comision con 10% de comision y 1 tarjeta de venta', function () {
-        let tarjetaVenta1 = new TarjetaVenta(500, "2018-03-22");
+        let tarjetaVenta1 = new TarjetaVenta(500, "2019-03-22");
         let calculadora = new CalculadoraPorComision(1000, 0.10, [tarjetaVenta1]);
         expect(calculadora.calcularSalario()).equal(1050);
     });
     
     it('calcular el salario para un empleado por comision con 10% de comision y mas de 1 tarjeta de venta', function () {
-        let tarjetaVenta1 = new TarjetaVenta(500, "2018-03-22");
-        let tarjetaVenta2 = new TarjetaVenta(900, "2018-03-23");
-        let tarjetaVenta3 = new TarjetaVenta(300, "2018-03-23");
+        let tarjetaVenta1 = new TarjetaVenta(500, "2019-03-22");
+        let tarjetaVenta2 = new TarjetaVenta(900, "2019-03-23");
+        let tarjetaVenta3 = new TarjetaVenta(300, "2019-03-23");
         let calculadora = new CalculadoraPorComision(1000, 0.10, [tarjetaVenta1, tarjetaVenta2, tarjetaVenta3]);
         expect(calculadora.calcularSalario()).equal(1170);
     });
